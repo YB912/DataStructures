504b1a4fee0a65e35ea01308fcd1c7c7a3e15892
YB912 <YB912TU@gmail.com>
2023-02-21 11:37:56 +0330

    Added and initialized SinglyLinkedList class

---
+++ SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.SinglyLinkedList
+{
+    internal class SinglyLinkedList<T> : ISinglyLinkedList<T>
+    {
+        public int Size()
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+        public int CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+        public T First()
+        {
+            throw new NotImplementedException();
+        }
+        public T Last()
+        {
+            throw new NotImplementedException();
+        }
+        public void AddFirst(T element)
+        {
+            throw new NotImplementedException();
+        }
+        public void AddLast(T element)
+        {
+            throw new NotImplementedException();
+        }
+        public T RemoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
