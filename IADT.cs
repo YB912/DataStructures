593d8a8f4082446ec5c0b5f76f6627e987e02162
YB912 <YB912TU@gmail.com>
2023-02-21 11:20:57 +0330

    Added and initialized IADT interface

---
+++ IADT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal interface IADT
+    {
+        int Size();
+        bool IsEmpty();
+        void Clear();
+    }
+}
