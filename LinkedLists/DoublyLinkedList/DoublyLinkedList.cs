b23523e1215296979532cd22cdf09a99f020a04a
YB912 <YB912TU@gmail.com>
2023-02-23 20:00:48 +0330

    Created and initialized empty DoublyLinkedList class and IDoublyLinkedList interface

---
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace DataStructures.LinkedLists
+{
+    internal class DoublyLinkedList<T> : IDoublyLinkedList<T> where T : class, ICloneable, IComparable
+    {
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+        public int Size()
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ LinkedLists/DoublyLinkedList/IDoublyLinkedList.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace DataStructures.LinkedLists
+{
+    internal interface IDoublyLinkedList<T> : IADT, ICloneable
+    {
+    }
+}
