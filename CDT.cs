26e6c29f14fb6892d3e5938eecc5a8c7a1d88aa3
YB912 <YB912TU@gmail.com>
2023-02-28 15:16:25 +0330

    Add concrete data type (CDT) class to reduce repetition

---
+++ CDT.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DataStructures
+{
+    internal abstract class CDT : IADT
+    {
+        protected internal int _size;
+        public int Size() { return _size; }
+        public bool IsEmpty() { return _size == 0; }
+        public virtual void Clear() { _size = 0; }
+    }
+}
