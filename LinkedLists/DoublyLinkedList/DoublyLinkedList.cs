4099917a8187aae837baa2b35e22d5fe8f2ebdcf
YB912 <YB912TU@gmail.com>
2023-03-03 00:37:17 +0330

    Override ToString() and fix Clone() in DoublyLinkedList and debug the class

--- LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.cs
@@ -8,27 +8,13 @@
         {
             _size = 0;
             _headerSentinel = new DoubleNode(null, null, null);
-            _tailersentinel = new DoubleNode(null, null, _headerSentinel);
+            _tailersentinel = new DoubleNode(null, _headerSentinel, null);
+            _headerSentinel.Next = _tailersentinel;
         }
         public void Clear()
         {
             base.Clear();
             _headerSentinel.Next = _tailersentinel.Prev = null;
-        }
-        public object Clone()
-        {
-            var output = new DoublyLinkedList<T>();
-            if (_size > 0)
-            {
-                output.AddFirst(_headerSentinel.Next.Element.Clone() as T);
-                var iterator = _headerSentinel.Next.Next;
-                while (iterator != null)
-                {
-                    output.AddLast(iterator.Element.Clone() as T);
-                    iterator = iterator.Next;
-                }
-            }
-            return output;
         }
         public T First() { return _headerSentinel.Next.Element; }
         public T Last() { return _tailersentinel.Prev.Element; }
@@ -36,6 +22,20 @@
         public void AddLast(T element) { AddBetween(element, _tailersentinel.Prev, _tailersentinel); }
         public T RemoveFirst() { return Remove(_headerSentinel.Next); }
         public T RemoveLast() { return Remove(_tailersentinel.Prev); }
+        public object Clone()
+        {
+            var output = new DoublyLinkedList<T>();
+            if (IsEmpty() == false)
+            {
+                var current = _headerSentinel.Next;
+                for (int  i = 0; i < _size; i++)
+                {
+                    output.AddLast(current.Element.Clone() as T);
+                    current = current.Next;
+                }
+            }
+            return output;
+        }
         public override bool Equals(object? obj)
         {
             if (obj == null) return false;
@@ -44,13 +44,29 @@
             if (_size != other._size) return false;
             var traverseThis = _headerSentinel.Next;
             var traverseOther = other._headerSentinel.Next;
-            while (traverseThis != null)
+            for (int i = 0; i < _size; i++)
             {
                 if (traverseThis.Element.Equals(traverseOther.Element) == false) return false;
                 traverseThis = traverseThis.Next;
                 traverseOther = traverseOther.Next;
             }
             return true;
+        }
+        public override string ToString()
+        {
+            string output = "{ ";
+            DoubleNode current = _headerSentinel.Next;
+            for (int i = 0; i < _size; i++)
+            {
+                output += current.Element.ToString();
+                if (i != _size - 1)
+                {
+                    output += ", ";
+                }
+                current = current.Next;
+            }
+            output += " }";
+            return output;
         }
         private void AddBetween(T element, DoubleNode prev, DoubleNode next) 
         {
@@ -77,15 +93,15 @@
                 Element = element;
                 Next = Prev = null;
             }
-            public DoubleNode(T element, DoubleNode next, DoubleNode prev)
+            public DoubleNode(T element, DoubleNode prev, DoubleNode next)
             {
                 Element = element;
-                Next = next;
                 Prev = prev;
+                Next = next;
             }
             public object Clone()
             {
-                return new DoubleNode(Element, Next, Prev);
+                return new DoubleNode(Element, Prev, Next);
             }
             public int CompareTo(object? obj)
             {
