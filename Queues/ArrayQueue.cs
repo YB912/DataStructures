b3c78adc8295b3251fc08544850ecff0c4fe1bce
YB912 <YB912TU@gmail.com>
2023-03-11 21:45:41 +0330

    Add and initialize ArrayQueue class and IQueue interface

---
+++ Queues/ArrayQueue.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace DataStructures.Queues
+{
+    internal class ArrayQueue<T> : CDT, IQueue<T> where T : class, ICloneable, IComparable
+    {
+        private const int DefaultCapacity = 1000;
+        private readonly T[] _buffer;
+        private int _head;
+        public override void Clear()
+        {
+            throw new NotImplementedException();
+        }
+        public void Enqueue(T element)
+        {
+            throw new NotImplementedException();
+        }
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+        public T First()
+        {
+            throw new NotImplementedException();
+        }
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Equals(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Queues/IQueue.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace DataStructures.Queues
+{
+    public interface IQueue<T> : IADT, ICloneable where T : class, ICloneable, IComparable
+    {
+        public void Enqueue(T element);
+        public T Dequeue();
+        public T First();
+    }
+}
